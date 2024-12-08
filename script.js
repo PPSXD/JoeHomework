c190a39a66ca44d6446b37c5d8e4e13197f5883c
PPSXD <raynerchung888@gmail.com>
2024-12-08 16:49:22 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const navBtn = document.getElementById("nav-mobile-toggle");
+const navLinks = document.getElementById("mobile-nav-list");
+
+navBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+function About(){
+    window.open(`about.html` , `_self`);
+}
+
+function Service(){
+    window.open(`service.html` , `_self`);
+}
+
+function Career(){
+    window.open(`WorkOfUs.html` , `_self`);
+}0
+
+function Contact(){
+    window.open(`contact-v2.html` , `_self`);
+}
